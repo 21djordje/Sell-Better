74d1a1a89bd62cf9e96002c3d8c4ceef18cb7559
djordje.sinikovic <djordjesinikovic@gmail.com>
2022-05-21 14:40:41 +0200

    hero changes

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,13 @@ $(document).ready(function () {
     var row = $(this).closest(".event-box");
     row.find(".acc-icon a").trigger("click");
   });
+
+  var listOfClasses = [
+    "color-scheme-1",
+    "color-scheme-2",
+    "color-scheme-3",
+    "color-scheme-4",
+  ];
+  var randomNum = Math.floor(Math.random() * listOfClasses.length);
+  $(".hero-desktop").addClass(listOfClasses[randomNum]);
 });
